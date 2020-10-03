60523b53d42a38d4527174763494e01b085dc46a
FrederikaKluiwstra <fjkluiwstra@yahoo.com>
2020-10-03 12:40:24 +0200

    Does this rock?

---
+++ ArrowFunctions-exercise.js
@@ -0,0 +1,9 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+let fivePlusSeven = () => 5 + 7;
+
+let myFunction = (a, b) => a + b;
+
+let addFive = a => a + 5;
+
+let createObject = () => ({ greeting: "hoi" });
