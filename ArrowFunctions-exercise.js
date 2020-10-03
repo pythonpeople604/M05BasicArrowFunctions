32040bb316400412c90b95743ef45fec9856ffad
pythonpeople604 <64478663+pythonpeople604@users.noreply.github.com>
2020-10-03 19:27:37 +0530

    Update ArrowFunctions-exercise.js

--- ArrowFunctions-exercise.js
+++ ArrowFunctions-exercise.js
@@ -7,3 +7,9 @@ let myFunction = (a, b) => a + b;
 let addFive = a => a + 5;
 
 let createObject = () => ({ greeting: "hoi" });
+
+let createObject = () => ({ 'GTA V is the best game'});
+
+var a = 4;
+var b = 10;
+let myMath = (a, b) => a + b;
